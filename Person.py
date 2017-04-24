35a5d9f4a821aa2c3dbd1fbcfb6f66991f03fdaa
Ju Ho Choi <rl2080vg@mnstate.edu>
2017-04-24 02:03:03 -0500

    add str and test the method

--- Person.py
+++ Person.py
@@ -4,6 +4,12 @@ class Person:
     self.fName = tmpfName
     self.lName = tmplName
     self.ID = lastIdUsed
+  
+  def __str__(self):
+    tmp = ''
+    tmp += self.fName + " " + self.lName + " " + str(self.ID) + "\n"
+    return tmp
+
 
 p1 = Person("Joe","Jones")
 
